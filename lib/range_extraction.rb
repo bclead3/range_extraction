a8d3779cb30a8316c4ae5bb005147bdbeb30c180
Ben Leadholm <bclead3@yahoo.com>
2022-02-04 06:04:29 -0600

    Re-tool logic, now handles all specs

--- lib/range_extraction.rb
+++ lib/range_extraction.rb
@@ -2,43 +2,73 @@ require 'byebug'
 
 class RangeExtraction
 
-  # .{"0"=>{:val=>-10},
-  #  "1"=>{:val=>-9, :prev_element=>-10, :diff=>1},
-  #  "2"=>{:val=>-8, :prev_element=>-9, :diff=>1, :prev_diff=>1},
-  #  "3"=>{:val=>-6, :prev_element=>-8, :diff=>2, :prev_diff=>1},
-  #  "4"=>{:val=>-3, :prev_element=>-6, :diff=>3, :prev_diff=>2},
-  #  "5"=>{:val=>-2, :prev_element=>-3, :diff=>1, :prev_diff=>3},
-  #  "6"=>{:val=>-1, :prev_element=>-2, :diff=>1, :prev_diff=>1},
-  #  "7"=>{:val=>0, :prev_element=>-1, :diff=>1, :prev_diff=>1},
-  #  "8"=>{:val=>1, :prev_element=>0, :diff=>1, :prev_diff=>1},
-  #  "9"=>{:val=>3, :prev_element=>1, :diff=>2, :prev_diff=>1},
-  #  "10"=>{:val=>4, :prev_element=>3, :diff=>1, :prev_diff=>2},
-  #  "11"=>{:val=>5, :prev_element=>4, :diff=>1, :prev_diff=>1},
-  #  "12"=>{:val=>7, :prev_element=>5, :diff=>2, :prev_diff=>1},
-  #  "13"=>{:val=>8, :prev_element=>7, :diff=>1, :prev_diff=>2},
-  #  "14"=>{:val=>9, :prev_element=>8, :diff=>1, :prev_diff=>1},
-  #  "15"=>{:val=>10, :prev_element=>9, :diff=>1, :prev_diff=>1},
-  #  "16"=>{:val=>11, :prev_element=>10, :diff=>1, :prev_diff=>1},
-  #  "17"=>{:val=>14, :prev_element=>11, :diff=>3, :prev_diff=>1},
-  #  "18"=>{:val=>15, :prev_element=>14, :diff=>1, :prev_diff=>3},
-  #  "19"=>{:val=>17, :prev_element=>15, :diff=>2, :prev_diff=>1},
-  #  "20"=>{:val=>18, :prev_element=>17, :diff=>1, :prev_diff=>2},
-  #  "21"=>{:val=>19, :prev_element=>18, :diff=>1, :prev_diff=>1},
-  #  "22"=>{:val=>20, :prev_element=>19, :diff=>1, :prev_diff=>1}}
-  # expected: "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
-  #      got: "-10--8,-6,-3-1,3-5,7-11,14-15,17-20"
+  # {"0"=>{:v=>-10, :p=>nil, :nxt=>-9, :f_d=>1, :d=>nil, :p_d=>nil, :p_p_d=>nil, :t_c=>0},
+  #  "1"=>{:v=>-9, :p=>-10, :nxt=>-8, :f_d=>1, :d=>1, :p_d=>-10, :p_p_d=>nil, :t_c=>1},
+  #  "2"=>{:v=>-8, :p=>-9, :nxt=>-6, :f_d=>2, :d=>1, :p_d=>1, :p_p_d=>-10, :t_c=>2},
+  #  "3"=>{:v=>-6, :p=>-8, :nxt=>-3, :f_d=>3, :d=>2, :p_d=>1, :p_p_d=>1, :t_c=>0},
+  #  "4"=>{:v=>-3, :p=>-6, :nxt=>-2, :f_d=>1, :d=>3, :p_d=>2, :p_p_d=>1, :t_c=>0},
+  #  "5"=>{:v=>-2, :p=>-3, :nxt=>-1, :f_d=>1, :d=>1, :p_d=>3, :p_p_d=>2, :t_c=>1},
+  #  "6"=>{:v=>-1, :p=>-2, :nxt=>0, :f_d=>1, :d=>1, :p_d=>1, :p_p_d=>3, :t_c=>2},
+  #  "7"=>{:v=>0, :p=>-1, :nxt=>1, :f_d=>1, :d=>1, :p_d=>1, :p_p_d=>1, :t_c=>3},
+  #  "8"=>{:v=>1, :p=>0, :nxt=>3, :f_d=>2, :d=>1, :p_d=>1, :p_p_d=>1, :t_c=>4},
+  #  "9"=>{:v=>3, :p=>1, :nxt=>4, :f_d=>1, :d=>2, :p_d=>1, :p_p_d=>1, :t_c=>0},
+  #  "10"=>{:v=>4, :p=>3, :nxt=>5, :f_d=>1, :d=>1, :p_d=>2, :p_p_d=>1, :t_c=>1},
+  #  "11"=>{:v=>5, :p=>4, :nxt=>7, :f_d=>2, :d=>1, :p_d=>1, :p_p_d=>2, :t_c=>2},
+  #  "12"=>{:v=>7, :p=>5, :nxt=>8, :f_d=>1, :d=>2, :p_d=>1, :p_p_d=>1, :t_c=>0},
+  #  "13"=>{:v=>8, :p=>7, :nxt=>9, :f_d=>1, :d=>1, :p_d=>2, :p_p_d=>1, :t_c=>1},
+  #  "14"=>{:v=>9, :p=>8, :nxt=>10, :f_d=>1, :d=>1, :p_d=>1, :p_p_d=>2, :t_c=>2},
+  #  "15"=>{:v=>10, :p=>9, :nxt=>11, :f_d=>1, :d=>1, :p_d=>1, :p_p_d=>1, :t_c=>3},
+  #  "16"=>{:v=>11, :p=>10, :nxt=>14, :f_d=>3, :d=>1, :p_d=>1, :p_p_d=>1, :t_c=>4},
+  #  "17"=>{:v=>14, :p=>11, :nxt=>15, :f_d=>1, :d=>3, :p_d=>1, :p_p_d=>1, :t_c=>0},
+  #  "18"=>{:v=>15, :p=>14, :nxt=>17, :f_d=>2, :d=>1, :p_d=>3, :p_p_d=>1, :t_c=>1},
+  #  "19"=>{:v=>17, :p=>15, :nxt=>nil, :f_d=>nil, :d=>2, :p_d=>1, :p_p_d=>3, :t_c=>0}}
   def solution(arr)
     diff_hash = diff_hasher(arr)
     build_str = ''
-    prev_prev_diff = nil
+    prev_track_count = 0
     diff_hash.each do |k, v|
-      build_str += v[:val].to_s if k == '0'
-      build_str += '' if v[:diff] && v[:diff] == 1 && v[:prev_diff] && v[:prev_diff] == 1
-      build_str += "-#{v[:prev_element]}" if v[:diff] && v[:diff] > 1 && v[:prev_diff] && v[:prev_diff] == 1 && prev_prev_diff == 1
-      build_str += "-#{v[:val]}" if k.to_i == (arr.size-1) && v[:prev_diff] && v[:prev_diff] == 1
-      build_str += ",#{v[:val]}" if v[:diff] && v[:diff] > 1
-      prev_prev_diff = v[:prev_diff] if v[:prev_diff] && v[:prev_diff] == 1
-      
+      build_str += v[:v].to_s if k == '0'
+      if v[:d] &&
+         v[:d] > 1 &&
+         v[:p_d] &&
+         v[:p_d] == 1 &&
+         v[:p_p_d] == 1
+        build_str += "-#{v[:p]}"
+      end
+      if k.to_i == (arr.size-1) &&
+         v[:d] &&
+         v[:d] == 1 &&
+         v[:p_d] &&
+         v[:p_d] == 1
+        build_str += "-#{v[:v]}"
+      end
+      if k.to_i == (arr.size-1) &&
+         v[:d] &&
+         v[:d] == 1 &&
+         v[:p_d] &&
+         v[:p_d] > 1
+        build_str += ",#{v[:v]}"
+      end
+      if v[:d] &&
+         v[:d] == 1 &&
+         v[:p_d] &&
+         v[:p_d] > 1 &&
+         v[:p_p_d] == 1 &&
+         v[:t_c] > 1
+        build_str += ",#{v[:v]}"
+      end
+      if v[:d] &&
+         v[:d] > 1
+        build_str += ",#{v[:v]}"
+      end
+      if v[:d] &&
+         v[:d] == 1 &&
+         v[:f_d] &&
+         v[:f_d] > 1 &&
+         v[:p_d] &&
+         v[:p_d] > 1
+        build_str += ",#{v[:v]}"
+      end
     end
     build_str
   end
@@ -46,26 +76,27 @@ class RangeExtraction
   def diff_hasher(arr)
     diff_h = {}
     prev_element = nil
-    prev_diff = nil
+    p_diff = nil
+    p_p_d = nil
+    track_count = 0
     arr.each_with_index do |element, arr_index|
-      diff_h[arr_index.to_s] = {}
-      diff_h[arr_index.to_s][:val] = element
-      if arr_index > 1
-        diff_h[arr_index.to_s][:prev_element] = prev_element
-        diff = element - prev_element.to_i
-        diff_h[arr_index.to_s][:diff] = diff
-        # byebug
-        diff_h[arr_index.to_s][:prev_diff] = prev_diff
-        prev_diff = diff
-      elsif arr_index > 0
-        diff_h[arr_index.to_s][:prev_element] = prev_element
-        diff = element - prev_element.to_i
-        diff_h[arr_index.to_s][:diff] = diff
-        # byebug
-        prev_diff = diff
-      end
+      s_index = arr_index.to_s
+      diff = element - prev_element.to_i
+
+      diff_h[s_index] = {}
+      diff_h[s_index][:v] = element
+      diff_h[s_index][:p] = prev_element
+      diff_h[s_index][:nxt] = arr[arr_index+1]
+      diff_h[s_index][:f_d] = arr[arr_index+1] ? arr[arr_index+1] - element : nil
+      diff_h[s_index][:d] = prev_element.nil? ? nil : diff
+      diff_h[s_index][:p_d] = p_diff
+      diff_h[s_index][:p_p_d] = p_p_d
+      diff_h[s_index][:t_c] = diff == 1 ? track_count+=1 : track_count = 0
+
+      p_p_d = p_diff
+      p_diff = diff
       prev_element = element
     end
-    pp diff_h
+    diff_h
   end
 end
--- spec/range_extraction_spec.rb
+++ spec/range_extraction_spec.rb
@@ -12,9 +12,41 @@ RSpec.describe RangeExtraction do
   end
 
   describe '#solution' do
+    let(:arr) { [-12, -10, -9, -8, -6, -3]}
+    it 'works for small range' do
+      expect(subject.solution(arr)).to eq('-12,-10--8,-6,-3')
+    end
+  end
+
+  describe '#solution' do
+    let(:arr) { [-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14] }
+    it 'works for full range' do
+      expect(subject.solution(arr)).to eq('-10--8,-6,-3-1,3-5,7-11,14')
+    end
+  end
+
+  describe '#solution' do
+    let(:arr) { [-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15] }
+    it 'works for full range' do
+      expect(subject.solution(arr)).to eq('-10--8,-6,-3-1,3-5,7-11,14,15')
+    end
+  end
+  describe '#solution' do
+    let(:arr) { [-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17] }
+    it 'works for full range' do
+      expect(subject.solution(arr)).to eq('-10--8,-6,-3-1,3-5,7-11,14,15,17')
+    end
+  end
+  describe '#solution' do
     let(:arr) { [-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20] }
     it 'works for full range' do
       expect(subject.solution(arr)).to eq('-10--8,-6,-3-1,3-5,7-11,14,15,17-20')
+    end
+  end
+  describe '#solution' do
+    let(:arr) { [-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20, 22] }
+    it 'works for full range' do
+      expect(subject.solution(arr)).to eq('-10--8,-6,-3-1,3-5,7-11,14,15,17-20,22')
     end
   end
 end
